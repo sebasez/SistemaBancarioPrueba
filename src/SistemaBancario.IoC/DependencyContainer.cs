911ec92ee26d636247e461e1a49036cf29f923a7
Sebastian Gomez <sebasez@outlook.com>
2023-03-30 12:17:44 -0500

    Adición de inversión de dependencias

---
+++ src/SistemaBancario.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SistemaBancario.Presenters;
+using SistemaBancario.Repository;
+using SistemaBancario.UseCases;
+
+namespace SistemaBancario.IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddRealEstateWeeloDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddRepositories(configuration);
+            services.AddUseCasesServices();
+            services.AddPresenters();
+            return services;
+        }
+    }
+}
